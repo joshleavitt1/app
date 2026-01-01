877bc36aff88785ebd7b6c13bff218d70404d561
joshleavitt1 <josh@joshleavitt.com>
2026-01-01 13:37:36 -0500

    d

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/app",
-  assetPrefix: "/app",
-  output: "export",
-  trailingSlash: true
-};
-
-export default nextConfig;
